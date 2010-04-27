66d3e755828f790aac1cb598c9fae03a5160cf8f
Samuel Kortas <samuel.Kortas@edf.fr>
2010-04-27 06:04:46 +0200

    sans les _drive_

--- factures/traite.bat
+++ factures/traite.bat
@@ -1,7 +1,5 @@
-PATH=c:\Program Files\EasyPHP1-8\php\;c:\Program Files\EasyPHP1-8\php\extensions\;e:\program Files\ghostgum\gsview;e:\Program Files\EasyPHP 2.0b1\php5\;e:\Program Files\EasyPHP 2.0b1\php5\ext\;e:\program Files\ghostgum\gsview;e\program Files\MiKTeX 2.6\miktex\bin;
-
-
-rem cd   "c:\Program Files\EasyPHP1-8\www\phpmyfactures\factures"
+PATH=c:\Program Files\EasyPHP1-8\php\;c:\Program Files\EasyPHP1-8\php\extensions\;c:\program Files\ghostgum\gsview;
+cd   "c:\Program Files\EasyPHP1-8\www\phpmyfactures\factures"
 php.exe traite.php > traite_out.txt
 rem copy c:\Oyak\facture.ps c:\Oyak\ToPrint\facture.ps
 echo "impression en cours"
