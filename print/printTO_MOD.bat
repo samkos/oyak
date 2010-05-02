f6d62414873b4f2dbd8386dd6a204875a4128abe
Samuel Kortas <samuel.Kortas@edf.fr>
2010-05-02 06:17:41 +0200

    travail sur les fichiers de commandes traite_xxx

---
+++ print/printTO_MOD.bat
@@ -0,0 +1,7 @@
+PATH=__DRIVE__:\program Files\__LATEX__\miktex\bin;C:\WINDOWS\system32;c:\windows;__DRIVE__:\program Files\ghostgum\gsview
+%gsprint -landscape  -printer %2 %1
+gsprint  -printer %2 %1
+echo "impression en cours"
+							
+
+
---
+++ print/print_MOD.bat
@@ -0,0 +1,5 @@
+PATH=__DRIVE__:\program Files\__LATEX__\miktex\bin;C:\WINDOWS\system32;c:\windows;__DRIVE__:\program Files\ghostgum\gsview;e:\program Files\ghostgum\gsview
+gsprint %1
+echo "impression en cours"
+							
+
