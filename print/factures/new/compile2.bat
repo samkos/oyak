d27ff1257c762323849870643a202b8171704e83
Samuel Kortas <samuel.Kortas@edf.fr>
2010-05-05 15:26:14 +0200

    cleaning continues

--- print/factures/new/compile2.bat
+++
@@ -1,6 +0,0 @@
-PATH=c:\program Files\MiKTeX 2.5\miktex\bin;C:\WINDOWS\system32;c:\windows;c:\program Files\ghostgum\gsview
-latex "\nonstopmode\input" all.ok2.tex
-dvips all.ok2
-copy all.ok2.ps all.ps
-
-
--- print/factures/new/traite_MOD.bat
+++
@@ -1,7 +0,0 @@
-PATH=c:\Program Files\EasyPHP1-8\php\;c:\Program Files\EasyPHP1-8\php\extensions\;__DRIVE__:\program Files\ghostgum\gsview;e:\Program Files\EasyPHP 2.0b1\php5\;e:\Program Files\EasyPHP 2.0b1\php5\ext\;e:\program Files\ghostgum\gsview;__DRIVE__\program Files\__LATEX__\miktex\bin;
-
-
-rem cd   "c:\Program Files\EasyPHP1-8\www\phpmyfactures\factures"
-php.exe traite.php > traite_out.txt
-rem copy c:\Oyak\facture.ps c:\Oyak\ToPrint\facture.ps
-echo "impression en cours"
