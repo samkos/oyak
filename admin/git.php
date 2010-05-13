f3444a823634e61be7a7058ea090fcdb3f5ac235
Samuel Kortas <samuel.Kortas@edf.fr>
2010-05-13 03:26:43 +0200

    modif affichage git command

--- admin/git.php
+++ admin/git.php
@@ -10,7 +10,7 @@ $commande="git $action";
 if ($comment) {
   $commande="git $action -m \"$comment\" ";
 }
-system("$commande > out");
+system("$commande > out 2>&1 ");
 $res=file("out");
 
 ?>
@@ -21,14 +21,13 @@ include("git_command.php");
 ?>
 <tr> <td colspan=6 align=center> 
   	<a href='index.php'>  Retour administration </a> </td> </tr>
-    <tr>
-    <td bgcolor="#99CCCC" colspan=6 align=center>  <b> gestion de configuration </b> </td> </tr> 
+    <tr> <tr> <td> &nbsp; </td> </tr>
     <td bgcolor="#99CCCC" colspan=6 align=center>  <b> <?php print $commande; ?></b> </td> </tr> 
     <tr >
-      <tr> <td colspan=6>
+      <tr> <td colspan=6><quote>
   <?php 
   print join($res,"<br>");
-  ?>
+  ?></quote>
   </td> <tr> <td align=center colspan=6> 
   	<a href='index.php'>  Retour administration </a> </td> </tr>
 </table>
