caaf27b43a4dc88376b9ec0a07ca9f3f90233e71
Samuel Kortas <samuel.Kortas@edf.fr>
2010-06-04 06:56:55 +0200

    printer demon  en friche...

--- print/impression/traite.bat
+++ print/impression/traite.bat
@@ -1,6 +1,6 @@
 @echo off
 PATH=c:\Program Files\EasyEasyPHP-5.3.2i\php\;c:\Program Files\EasyPHP-5.3.2i\php\ext\;c:\program Files\ghostgum\gsview;c:\program Files\MiKTeX 2.6\miktex\bin;
 c:
-cd "\Program Files\EasyPHP-5.3.2i\www\phpmyfactures\print\impression"
-"c:\Program Files\EasyPHP-5.3.2i\php\php.exe" traite.php  traite.php --nohtml=1
+cd "\Program Files\EasyPHP-5.3.2i\www\oyak\print\impression"
+"c:\Program Files\EasyPHP-5.3.2i\php\php.exe" traite.php   --nohtml=1
 
