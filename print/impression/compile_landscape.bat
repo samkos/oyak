0d2d67ac1407678ecb679adc5f645c5b0d3b6a8a
Samuel Kortas <samuel.Kortas@edf.fr>
2010-07-15 07:58:30 +0200

    modif en attente

--- print/impression/compile_landscape.bat
+++ print/impression/compile_landscape.bat
@@ -1,5 +1,5 @@
 call ../../inc/path.bat
 del all_landscape.pdf
-pdflatex all_landscape.tex
+pdflatex  -interaction nonstopmode all_landscape.tex
 copy all_landscape.pdf c:\Oyak\general_landscape.pdf
 copy all_landscape.pdf c:\Oyak\ToPrint\general_landscape.pdf
