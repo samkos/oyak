cea5e61d6d5cc9a2a6d53c673b41b63f1238068b
Samuel KORTAS <samuel.kortas@gmail.com>
2010-07-17 10:11:11 +0200

    trace de la performance dans facture/perf

--- print/factures/compile.bat
+++ print/factures/compile.bat
@@ -2,9 +2,9 @@
 call ../../inc/path.bat
 if exist all.pdf del all.pdf
 if exist erreur.log del erreur.log
-echo | time | find "actuelle"
+echo | time | find "actuelle" >> perf.out
 pdflatex  -interaction nonstopmode  all.tex > erreur.log
-echo | time | find "actuelle"
+echo | time | find "actuelle" >> perf.out
 if  exist all.pdf  goto ok
 msg /time:5 * fichier all.pdf inexistant, une erreur est survenue a son visionnement
 echo !
