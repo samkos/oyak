cd15c0f9ae6bcead6e5343801ce45da29ed395bc
Samuel KORTAS <samuelkortas@gmail.com>
2012-11-04 05:22:00 +0100

    1ere creation pdf ok

--- tools_pdf/index.php
+++ tools_pdf/index.php
@@ -7,5 +7,35 @@ header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
-header("Location: ../");
-exit;
+
+require('fpdf.php');
+
+// mode paysage, a4, etc.
+
+$pdf=new FPDF('L','mm','A4');
+$pdf->Open();
+
+// champs facultatifs
+
+$pdf->SetAuthor('Hyperion - Ben Hermans');
+$pdf->SetCreator('CygnusEd 4.21 & Fpdf');
+$pdf->SetTitle('Amiwest AmigaOS 4 Presentation');
+$pdf->SetSubject('Remix by bIgdAn');
+
+// on charge les 83 gfx...
+
+$pdf->SetMargins(0,0);
+for ($i=1; $i<4; $i++) {
+$pdf->AddPage();
+//$pdf->Image('Diapositive'.$i.'.JPG',0,0,297,210,'JPEG');
+$pdf->Image('../print/factures/persepolis.jpg',0,0,297,210,'JPEG');
+}
+
+// Et on affiche le pdf généré... (ou on le sauvegarde en local)
+//$pdf->Output(); pour afficher sur votre navigateur
+
+$pdf->Output('/tmp/results.pdf');
+
+?>
+
+
