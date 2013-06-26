f6aa24c1d08a24c256972c2f046ae5a4e69d5567
Samuel Kortas <samuel.kortas@edf.fr>
2013-06-26 16:34:32 +0200

    cgi directory renamed

--- device/appli/server.py
+++ device/appli/server.py
@@ -8,7 +8,7 @@ handler = CGIHTTPServer.CGIHTTPRequestHandler
 server_address = ("", 7777)
 #handler.cgi_directories = ["/home/samy/GIT/OYAK/device/appli/"]
  
-handler.cgi_directories = ["/cgi-bin"]
+handler.cgi_directories = ["/cgi"]
  
 httpd = server(server_address, handler)
 httpd.serve_forever()
